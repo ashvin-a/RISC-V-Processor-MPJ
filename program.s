72b82aadfe7fce33ba71fb3a45af8dc0fcc9dc73
sreeraj <kannakaranko@wisc.edu>
2025-11-01 16:40:51 -0500

    Update instructions to Phase 5 spec

--- program.s
+++ program.s
@@ -1,12 +1,30 @@
 .global _start
 _start:
-addi x1, x0, 5
-addi x2, x0, 4
-addi x3, x0, 9
-addi x4, x0, 3
-add x30, x1, x2
-sw x1,4(x2)
-addi x1, x0, 6
-addi x5, x0, 5
-addi x6, x0, 4
-addi x7, x0, 8
+L0: addi x7, x0, 0xf
+L1: addi x1, x0, 10
+addi x0, x0, 0
+addi x0, x0, 0
+L2: addi x2, x1, 1
+addi x0, x0, 0
+addi x0, x0, 0
+L3: addi x3, x2, 1
+addi x0, x0, 0
+addi x0, x0, 0
+L4: addi x4, x3, 1
+addi x0, x0, 0
+addi x0, x0, 0
+L5: addi x5, x4, 1
+addi x0, x0, 0
+addi x0, x0, 0
+L6:  addi x6, x5, 1
+addi x0, x0, 0
+addi x0, x0, 0
+L7:  beq x6, x7, L10
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+L8:  lui a0, 0xdead
+L9:  ebreak
+L10: lui a0, 0x1
+L11: ebreak
+
