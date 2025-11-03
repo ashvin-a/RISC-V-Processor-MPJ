31b3bb8418cc684a923d0b488b7fb0e05ac3f0e1
sreeraj <kannakaranko@wisc.edu>
2025-11-03 17:29:25 -0600

    Add working instructions for pipeline test

--- program.s
+++ program.s
@@ -1,30 +1,25 @@
 .global _start
 _start:
-L0: addi x7, x0, 0xf
-L1: addi x1, x0, 10
-addi x0, x0, 0
-addi x0, x0, 0
-L2: addi x2, x1, 1
-addi x0, x0, 0
-addi x0, x0, 0
-L3: addi x3, x2, 1
-addi x0, x0, 0
-addi x0, x0, 0
-L4: addi x4, x3, 1
-addi x0, x0, 0
-addi x0, x0, 0
-L5: addi x5, x4, 1
-addi x0, x0, 0
-addi x0, x0, 0
-L6:  addi x6, x5, 1
-addi x0, x0, 0
-addi x0, x0, 0
-L7:  beq x6, x7, L10
-addi x0, x0, 0
-addi x0, x0, 0
-addi x0, x0, 0
-L8:  lui a0, 0xdead
-L9:  ebreak
-L10: lui a0, 0x1
-L11: ebreak
-
+addi x1,x0,4
+addi x2,x0,0
+addi x7,x0,0x8
+addi x11,x0,0x8
+addi x12,x0,0x000003FF
+addi x0,x0,0
+addi x0,x0,0
+addi x0,x0,0
+sw x7,0(x2)
+sw x11,4(x1)
+sw x12,8(x1)
+addi x0,x0,0
+addi x0,x0,0
+lw x1,0(x2)
+addi x0,x0,0
+addi x0,x0,0
+addi x0,x0,0
+addi x0,x0,0
+lw x3,4(x1)
+addi x0,x0,0
+addi x0,x0,0
+add x5,x3,x3
+ebreak
