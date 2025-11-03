e77ab94a6e2f5c19425f015df85bdd1cbf292817
ashvin <ashvinanilkumarsh@gmail.com>
2025-11-02 20:01:14 -0600

    Add comments for clarity and update code to add MEM to MEM forwarding block.

--- forwarding_unit.v
+++ forwarding_unit.v
@@ -9,24 +9,34 @@ module forwarding_unit (
     input wire[4:0] i_rd_MEMWB,
     input wire i_clu_RegWrite_EXMEM,
     input wire i_clu_RegWrite_MEMWB,
-    input wire i_clu_MemWrite,
-    input wire i_clu_MemWrite,
+    input wire i_clu_MemWrite_EXMEM,
+    input wire i_clu_MemRead_EXMEM, // Doubt on whether its EXMEM or MEMWB. But does it matter?
     output wire[1:0] o_forward_A, 
-    output wire[1:0] o_forward_B
+    output wire[1:0] o_forward_B,
+    output wire o_forward_store  //? We might need this solely for handling MEM-to-MEM
 );
 
+// Will these things hold up for Jal and branch things?
 assign o_forward_A = ((i_clu_RegWrite_EXMEM == 1 && i_rd_EXMEM != 0) ? 
-                            (i_rd_EXMEM == i_rs1_IDEX_data) ?  
+                            (i_rd_EXMEM == i_rs1_IDEX_data) ?  // EX-to-EX Forwarding for rs1
                                 (2b'10) :
                       (i_clu_RegWrite_MEMWB == 1 && i_rd_MEMWB != 0) ?
-                            (i_rd_MEMWB == i_rs1_IDEX_data) ?  
+                            (i_rd_MEMWB == i_rs1_IDEX_data) ?  // MEM-to-EX Forwarding for rs1
                                 (2b'01))
 
 assign o_forward_B = ((i_clu_RegWrite_EXMEM == 1 && i_rd_EXMEM != 0) ?
-                            (i_rd_EXMEM == i_rs2_IDEX_data) ?
+                            (i_rd_EXMEM == i_rs2_IDEX_data) ?  // EX-to-EX Forwarding for rs2
                                 (2b'10):
                         (i_clu_RegWrite_MEMWB == 1 && i_rd_MEMWB != 0) ?
-                            (i_rd_MEMWB == i_rs2_IDEX_data) ?  
+                            (i_rd_MEMWB == i_rs2_IDEX_data) ?  // MEM-to-EX Forwarding for rs2
                                 (2b'01))
+
+// MEM-to-MEM Forwarding
+if (i_clu_MemRead_EXMEM == 1) && (i_clu_MemWrite_EXMEM == 1) begin
+    o_forward_store = 1
+    // This condition is incomplete. Need to check rs1 and rs2 data and other stuffs 🚶‍♂️.
+    // TODO: Add conditions to prioritize EX/MEM over MEM/WB so that we get the latest data.
+end
+
 
 endmodule
