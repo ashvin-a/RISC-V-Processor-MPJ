cf50724c5a11c90ef01d92cdfa3b58e9378cf56e
ashvin <ashvinanilkumarsh@gmail.com>
2025-12-02 18:42:00 -0600

    Update script for better readability.

--- extra_credits/assembly_to_hex.py
+++ extra_credits/assembly_to_hex.py
@@ -1,7 +1,9 @@
 from riscv_converter import RiscvAssembler
+# Just add your locations here and run script.
+ASSEMBLY_FILE_LOCATION = "extra_credits/bubblesort_large.asm"
+OUTPUT_LOCATION = "extra_credits/output.mem"
 
-
-with open("extra_credits/bubblesort_large.asm") as f:
+with open(ASSEMBLY_FILE_LOCATION, "r") as f:
     code = f.read()
 
 
@@ -12,5 +14,5 @@ output_code = ""
 for code in machine_code_hex:
     output_code = output_code + f"{code[6:] + code[4:6] +  code[2:4] + code[:2]}\n"
 
-with open("extra_credits/output.mem", "w") as f:
+with open(OUTPUT_LOCATION, "w") as f:
     f.write(output_code)
