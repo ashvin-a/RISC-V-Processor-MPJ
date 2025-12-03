3b49d555858149b796822fc897ca244e98a8182d
ashvin <ashvinanilkumarsh@gmail.com>
2025-12-02 18:49:08 -0600

    Update script to get output in desired pattern.

--- extra_credits/assembly_to_hex.py
+++ extra_credits/assembly_to_hex.py
@@ -12,7 +12,7 @@ machine_code_hex = assembler.assemble()
 
 output_code = ""
 for code in machine_code_hex:
-    output_code = output_code + f"{code[6:] + code[4:6] +  code[2:4] + code[:2]}\n"
+    output_code = output_code + f"{code[:2]}\n" + f"{code[2:4]}\n" + f"{code[4:6]}\n" + f"{code[6:]}\n"
 
 with open(OUTPUT_LOCATION, "w") as f:
     f.write(output_code)
